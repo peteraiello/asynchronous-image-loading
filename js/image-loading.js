89201ee49036c4ef34831d0ee0fdeb5da58c4a15
Peter Aiello <peter.aiello89@gmail.com>
2019-11-26 22:02:40 +0000

    add callback function

--- js/image-loading.js
+++ js/image-loading.js
@@ -1,17 +1,31 @@
 /* Asynchronous image loading experiment
-Tutorial: https://blog.teamtreehouse.com/learn-asynchronous-image-loading-javascript
- */
+Tutorials:
+Asynchonous image loading: https://blog.teamtreehouse.com/learn-asynchronous-image-loading-javascript
+Understanding callback functions: https://codeburst.io/javascript-what-the-heck-is-a-callback-aba4da2deced
+*/
 
 // target the first image on page.
 
-let image = document.images[0];
+let image1 = document.getElementById("image1");
+let image2 = document.getElementById("image2");
 
 let downloadingImage = new Image();
 
+function loadImg(){
+    // stimulate a code delay
+    setTimeout( function(){
+      console.log('2 seconds is up!');
+      image1.src = "img/dancing-house-hi-quality.jpg";
+    }, 2000);
+}
 
-downloadingImage.onload = function(){
-    console.log("The image has loaded!");
-    image.src = this.src;
-};
+function loadImg2(){
+  // stimulate a code delay
+  setTimeout( function(){
+    console.log('4 seconds is up!');
+    image2.src = "img/dancing-house-hi-quality.jpg";
+  }, 4000);
+}
 
-downloadingImage.src ="img/dancing-house-hi-quality.jpg";
+loadImg();
+loadImg2();
